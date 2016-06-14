6005ba64332bb13d1d96f67da1c61b6d7386bd21
Peer Kokai <peerkokai@mail.de>
2016-06-14 12:04:16 +0200

    index

--- index.php
+++ index.php
@@ -14,3 +14,56 @@
 }
 ?>
 
+<!DOCTYPE html>
+
+<html lang="de">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta charset="utf-8" />
+
+        <title>Willkommen bei Home Drinks</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+       
+
+        <link href="~/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <link rel="stylesheet" href="CSS/StyleSheet.css">
+        <script src="JavaScript/Script.js"></script>
+        
+    </head>
+<!---- Wechsel zwischen Head und Body Bereich------------------------------------------------------------------------------------------------------>
+    <body>
+    <!---- Hier ist die Menüleiste--->
+    <nav id="navigation" class="clearfix">
+        <ul>
+            <li><a href="startseite.cshtml">Startseite</a></li>
+            <li><a href="favoriten.cshtml">Favoriten</a></li>
+            <li><a href="einkaufsliste">Einkaufliste</a></li>
+            <li><a href="cocktailliste">Cocktail-Liste</a></li>
+            <li><a href="#">Log-In</a></li>
+        </ul>
+    </nav>
+    <!---- Hier endet die Menüleiste--->
+
+
+
+    <!---- Hier ist die Kategorieauswahl--->
+    <button class="buttoncategory" onclick="location.href='Auswahlkategorien/category_level1.cshtml'">Zutaten hinzufügen +</button>
+    <!---- Hier endet die Kategorieauswahl--->
+    @*<!---- Suchfeld--->
+
+    
+        <input type="text" name="search" list="search" id="searchfield">
+
+            <datalist id="search">    
+                @foreach( var row in data){
+                 <option value="@row.Name"></option>
+                }
+            </datalist>
+        </input>
+
+        <button id="btnName">Click me!</button>
+   
+    <!---- Suchfeld--->*@ 
+
+    </body>
+</html>
